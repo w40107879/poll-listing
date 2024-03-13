3a809164633dc2717e9589d1b56d1d9be5786d72
WilliamLiao <w04107879@gmail.com>
2024-03-13 22:56:16 +0800

    [backend] add eslint

--- poll-listing-api/.eslintrc.cjs
+++ poll-listing-api/.eslintrc.cjs
@@ -1,56 +1,56 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  overrides: [],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: 'backend/tsconfig.json',
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  rules: {
-    'no-console': 0,
-    'no-plusplus': 0,
-    'import/prefer-default-export': 0,
-    'arrow-body-style': 0,
-    'no-param-reassign': [
-      2,
-      {
-        props: false,
-      },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'airbnb',
+      'airbnb-typescript',
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'plugin:@typescript-eslint/recommended',
     ],
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': [
-      2,
-      {
-        args: 'none',
-      },
-    ],
-    'class-methods-use-this': 'off',
-    'max-classes-per-file': 'off',
-    'no-restricted-syntax': 'off',
-    '@typescript-eslint/no-throw-literal': 'off',
-    'guard-for-in': 'off',
-    'no-continue': 'off',
-    'no-await-in-loop': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: 'backend/tsconfig.json',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      'no-console': 0,
+      'no-plusplus': 0,
+      'import/prefer-default-export': 0,
+      'arrow-body-style': 0,
+      'no-param-reassign': [
+        2,
+        {
+          props: false,
+        },
+      ],
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          js: 'never',
+          ts: 'never',
+          tsx: 'never',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        {
+          args: 'none',
+        },
+      ],
+      'class-methods-use-this': 'off',
+      'max-classes-per-file': 'off',
+      'no-restricted-syntax': 'off',
+      '@typescript-eslint/no-throw-literal': 'off',
+      'guard-for-in': 'off',
+      'no-continue': 'off',
+      'no-await-in-loop': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  };
--- poll-listing-api/controllers/answer.ts
+++ poll-listing-api/controllers/answer.ts
@@ -20,43 +20,4 @@ export class AnswerController extends Controller {
 		const { id } = body;
 		return await this.answerService.increaseVote(id);
 	}
-
 }
-
-// export const createVotes = (req: any, res: any) => {
-//   const { id } = req.params;
-//   const { option } = req.body;
-
-// 	const filePath = resolve(__dirname, "../db/polls-vote.json");
-//   // Read the existing JSON file
-//   readFile(filePath, 'utf8', (err, data) => {
-//     if (err) {
-//       console.error('Error reading file:', err);
-//       return res.status(500).send('Internal Server Error');
-//     }
-
-//     // Parse the JSON data
-//     const votes = JSON.parse(data);
-
-//     // Add the new vote to the votes array
-//     votes.votes.push({
-//       id: votes.votes.length + 1, // Assign a new ID (you might want to improve this logic)
-//       polls_id: parseInt(id),
-//       answers_id: parseInt(option)
-//     });
-
-//     // Convert the votes object back to JSON
-//     const updatedData = JSON.stringify(votes, null, 2); // 2 spaces for indentation
-
-//     // Write the updated JSON data back to the file
-//     writeFile(filePath, updatedData, 'utf8', (err) => {
-//       if (err) {
-//         console.error('Error writing file:', err);
-//         return res.status(500).send('Internal Server Error');
-//       }
-
-//       console.log('Vote added successfully:', { id, option });
-//       res.status(200).send('Poll submitted successfully');
-//     });
-//   });
-// }
